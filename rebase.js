201e22250f4b8d478187ee1c4fd212e294962bec
Marijn Haverbeke <marijn@haverbeke.nl>
2016-07-01 15:45:01 +0200

    Use a cleaner interface for partially mapping through a remapping

--- rebase.js
+++ rebase.js
@@ -3,13 +3,12 @@
 // redo them.
 function rebaseSteps(transform, steps, inverted, inside) {
   for (let i = inverted.length - 1; i >= 0; i--) transform.step(inverted[i])
-  transform.mapping.mapFrom = inverted.length
   for (let i = 0; i < inside.length; i++) transform.step(inside[i])
-  for (let i = 0; i < steps.length; i++) {
-    let mapped = steps[i].map(transform.mapping)
-    transform.mapping.mapFrom--
+  for (let i = 0, mapFrom = inverted.length; i < steps.length; i++) {
+    let mapped = steps[i].map(transform.mapping.slice(mapFrom))
+    mapFrom--
     if (mapped && !transform.maybeStep(mapped).failed)
-      transform.mapping.setMirror(transform.mapping.mapFrom, transform.steps.length - 1)
+      transform.mapping.setMirror(mapFrom, transform.steps.length - 1)
   }
   return inverted.length + inside.length
 }
