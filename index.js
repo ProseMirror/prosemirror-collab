91894e8d428906a6f641493c221d51b114be2ba2
Marijn Haverbeke <marijn@haverbeke.nl>
2016-08-31 23:02:55 +0200

    Add documentation to collab, history, and keymap modules

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ exports.rebaseSteps = rebaseSteps
 // ::- This state field accumulates changes that have to be sent to
 // the central authority in the collaborating group and makes it
 // possible to integrate changes made by peers into our local
-// document. It is created and attached to the editor when the
-// [plugin](#collab.collab) is enabled, and will be available as the
-// `collab` field in the resulting editor state.
+// document. It is defined by the [plugin](#collab.collab), and will
+// be available as the `collab` field in the resulting editor state.
 class CollabState {
   constructor(version, unconfirmed) {
     // : number
@@ -74,10 +73,33 @@ function receiveAction(state, steps, clientIDs, ourID) {
 // Creates a plugin that enables the collaborative editing framework
 // for the editor.
 //
-// You can pass a `version` option, which determines the starting
-// version number of the collaborative editing, and defaults to 0.
+//   options::- An optional set of options
+//
+//     version:: ?number
+//     The starting version number of the collaborative editing.
+//     Defaults to 0.
+//
+//     clientID:: ?number
+//     This client's ID. Defaults to a random 32-bit number.
+//
+//   return::-
+//   The returned plugin object provides these additional properties:
+//
+//     clientID:: number
+//     The client ID.
+//
+//     receiveAction:: (state: EditorState, steps: [Step], clientIDs: [number]) → TransformAction
+//     Create an action that represents a set of new steps received
+//     from the authority. Applying this action moves the state
+//     forward along with the authority's view of the document.
+//
+//     sendableSteps:: () → ?{version: number, steps: [Step], clientID: number}
+//     Provides the data describing the editor's unconfirmed steps. The
+//     version and array of steps are the things you'd send to the
+//     central authority. Returns null when there is nothing to send.
 exports.collab = function(options) {
-  const clientID = options && options.clientID || Math.floor(Math.random() * 0xFFFFFFFF)
+  const clientID = options && options.clientID
+  if (clientID == null) clientID =  Math.floor(Math.random() * 0xFFFFFFFF)
 
   return {
     stateFields: {
@@ -100,10 +122,6 @@ exports.collab = function(options) {
       return receiveAction(state, steps, clientIDs, clientID)
     },
 
-    // :: () → ?{version: number, steps: [Step], clientID: number}
-    // Provides the data describing the editor's unconfirmed steps. The
-    // version and array of steps are the things you'd send to the
-    // central authority. Returns null when there is nothing to send.
     sendableSteps(state) {
       if (state.collab.unconfirmed.length == 0) return null
       return {
