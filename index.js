bbdce766cc5df1145e903d4afa51699142c314ea
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-01 12:40:21 +0200

    Wrap plugin objects in a class
    Make plugin helper functions top-level

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const {Plugin} = require("../state")
 const RopeSequence = require("rope-sequence")
 
 const {rebaseSteps} = require("./rebase")
@@ -7,11 +8,11 @@ exports.rebaseSteps = rebaseSteps
 // for collaborative editing can be hooked. See [this
 // guide](guide/collab.html) for more details and an example.
 
-// ::- This state field accumulates changes that have to be sent to
-// the central authority in the collaborating group and makes it
-// possible to integrate changes made by peers into our local
-// document. It is defined by the [plugin](#collab.collab), and will
-// be available as the `collab` field in the resulting editor state.
+// This state field accumulates changes that have to be sent to the
+// central authority in the collaborating group and makes it possible
+// to integrate changes made by peers into our local document. It is
+// defined by the plugin, and will be available as the `collab` field
+// in the resulting editor state.
 class CollabState {
   constructor(version, unconfirmed) {
     // : number
@@ -41,7 +42,7 @@ function unconfirmedFrom(transform, start = 0) {
 // Will recognize its own changes, and confirm unconfirmed steps as
 // appropriate. Remaining unconfirmed steps will be rebased over
 // remote steps.
-function receiveAction(state, steps, clientIDs, ourID) {
+function makeReceiveAction(state, steps, clientIDs, ourID) {
   let collab = state.collab
   let version = collab.version + steps.length
 
@@ -68,7 +69,28 @@ function receiveAction(state, steps, clientIDs, ourID) {
   return transform.action({rebased: nUnconfirmed, addToHistory: false, newCollabState, interaction: false})
 }
 
-// :: (?Object) → Object
+const plugin = new Plugin({
+  stateFields: {
+    collab: {
+      init: (_, state) => new CollabState(plugin.find(state).config.version, RopeSequence.empty),
+      applyAction({collab}, action) {
+        if (action.type == "transform")
+          return action.newCollabState ||
+          new CollabState(collab.version, collab.unconfirmed.append(unconfirmedFrom(action.transform)))
+        if (action.type == "collabConfirm")
+          return action.collabState
+        return collab
+      }
+    }
+  },
+
+  config: {
+    version: 0,
+    clientID: -1
+  }
+})
+
+// :: (?Object) → Plugin
 //
 // Creates a plugin that enables the collaborative editing framework
 // for the editor.
@@ -81,55 +103,35 @@ function receiveAction(state, steps, clientIDs, ourID) {
 //
 //     clientID:: ?number
 //     This client's ID. Defaults to a random 32-bit number.
-//
-//   return::-
-//   The returned plugin object provides these additional properties:
-//
-//     clientID:: number
-//     The client ID.
-//
-//     receiveAction:: (state: EditorState, steps: [Step], clientIDs: [number]) → TransformAction
-//     Create an action that represents a set of new steps received
-//     from the authority. Applying this action moves the state
-//     forward along with the authority's view of the document.
-//
-//     sendableSteps:: (state: EditorState) → ?{version: number, steps: [Step], clientID: number}
-//     Provides the data describing the editor's unconfirmed steps. The
-//     version and array of steps are the things you'd send to the
-//     central authority. Returns null when there is nothing to send.
-function collab(options) {
-  let clientID = options && options.clientID
+function collab(config) {
+  let clientID = config && config.clientID
   if (clientID == null) clientID =  Math.floor(Math.random() * 0xFFFFFFFF)
 
-  return {
-    stateFields: {
-      collab: {
-        init: () => new CollabState(options && options.version || 0, RopeSequence.empty),
-        applyAction({collab}, action) {
-          if (action.type == "transform")
-            return action.newCollabState ||
-              new CollabState(collab.version, collab.unconfirmed.append(unconfirmedFrom(action.transform)))
-          if (action.type == "collabConfirm")
-            return action.collabState
-          return collab
-        }
-      }
-    },
-
-    clientID,
-
-    receiveAction(state, steps, clientIDs) {
-      return receiveAction(state, steps, clientIDs, clientID)
-    },
-
-    sendableSteps(state) {
-      if (state.collab.unconfirmed.length == 0) return null
-      return {
-        version: state.collab.version,
-        steps: state.collab.unconfirmed.map(s => s.step),
-        clientID
-      }
-    }
-  }
+  let reconf = {clientID}
+  if (config && config.version != null) reconf.version = config.version
+  return plugin.configure(reconf)
 }
 exports.collab = collab
+
+// :: (state: EditorState, steps: [Step], clientIDs: [number]) → Action
+// Create an action that represents a set of new steps received from
+// the authority. Applying this action moves the state forward along
+// with the authority's view of the document.
+function receiveAction(state, steps, clientIDs) {
+  return makeReceiveAction(state, steps, clientIDs, plugin.find(state).config.clientID)
+}
+exports.receiveAction = receiveAction
+
+// :: (state: EditorState) → ?{version: number, steps: [Step], clientID: number}
+// Provides the data describing the editor's unconfirmed steps. The
+// version and array of steps are the things you'd send to the
+// central authority. Returns null when there is nothing to send.
+function sendableSteps(state) {
+  if (state.collab.unconfirmed.length == 0) return null
+  return {
+    version: state.collab.version,
+    steps: state.collab.unconfirmed.map(s => s.step),
+    clientID: plugin.find(state).config.clientID
+  }
+}
+exports.sendableSteps = sendableSteps
