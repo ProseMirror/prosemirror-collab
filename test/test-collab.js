18821d197203a75a665ec871acae9cbc20bad73b
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-06 16:24:05 +0200

    Port the collab tests to Mocha

---
+++ test/test-collab.js
@@ -0,0 +1,192 @@
+const {EditorState, Selection} = require("prosemirror-state")
+const {history, undo, redo} = require("prosemirror-history")
+const {schema, sameDoc, doc, p} = require("prosemirror-model/test/build")
+const ist = require("ist")
+
+const {collab, receiveAction, sendableSteps} = require("../src/collab")
+
+const histPlugin = history.configure({preserveItems: true})
+
+class DummyServer {
+  constructor(doc, n = 2) {
+    this.states = []
+    for (let i = 0; i < n; i++)
+      this.states.push(EditorState.create({doc, schema, plugins: [histPlugin, collab()]}))
+    this.steps = []
+    this.clientIDs = []
+    this.delayed = []
+  }
+
+  sync(n) {
+    let state = this.states[n], version = state.collab.version
+    if (version != this.steps.length)
+      this.states[n] = state.applyAction(receiveAction(state, this.steps.slice(version), this.clientIDs.slice(version)))
+  }
+
+  send(n) {
+    let sendable = sendableSteps(this.states[n])
+    if (sendable && sendable.version == this.steps.length) {
+      this.steps = this.steps.concat(sendable.steps)
+      for (let i = 0; i < sendable.steps.length; i++) this.clientIDs.push(sendable.clientID)
+    }
+  }
+
+  broadcast(n) {
+    if (this.delayed.indexOf(n) > -1) return
+    this.sync(n)
+    this.send(n)
+    for (let i = 0; i < this.states.length; i++) if (i != n) this.sync(i)
+  }
+
+  update(n, f) {
+    this.states[n] = this.states[n].applyAction(f(this.states[n]))
+    this.broadcast(n)
+  }
+
+  type(n, text, pos) {
+    this.update(n, s => s.tr.insertText(text, pos || s.selection.head).action())
+  }
+
+  undo(n) {
+    undo(this.states[n], a => this.update(n, () => a))
+  }
+
+  redo(n) {
+    redo(this.states[n], a => this.update(n, () => a))
+  }
+
+  conv(d) {
+    if (typeof d == "string") d = doc(p(d))
+    this.states.forEach((state, i) => ist(state.doc, d, sameDoc))
+  }
+
+  delay(n, f) {
+    this.delayed.push(n)
+    f()
+    this.delayed.pop()
+    this.broadcast(n)
+  }
+}
+
+function sel(near) {
+  return s => Selection.near(s.doc.resolve(near)).action()
+}
+function closeHist() { return {type: "historyClose"} }
+
+describe("collab", () => {
+  it("converges for simple changes", () => {
+    let s = new DummyServer
+    s.type(0, "hi")
+    s.type(1, "ok", 3)
+    s.type(0, "!", 5)
+    s.type(1, "...", 1)
+    s.conv("...hiok!")
+  })
+
+  it("converges for multiple local changes", () => {
+    let s = new DummyServer
+    s.type(0, "hi")
+    s.delay(0, () => {
+      s.type(0, "A")
+      s.type(1, "X")
+      s.type(0, "B")
+      s.type(1, "Y")
+    })
+    s.conv("hiXYAB")
+  })
+
+  it("converges with three peers", () => {
+    let s = new DummyServer(null, 3)
+    s.type(0, "A")
+    s.type(1, "U")
+    s.type(2, "X")
+    s.type(0, "B")
+    s.type(1, "V")
+    s.type(2, "C")
+    s.conv("AUXBVC")
+  })
+
+  it("converges with three peers with multiple steps", () => {
+    let s = new DummyServer(null, 3)
+    s.type(0, "A")
+    s.delay(1, () => {
+      s.type(1, "U")
+      s.type(2, "X")
+      s.type(0, "B")
+      s.type(1, "V")
+      s.type(2, "C")
+    })
+    s.conv("AXBCUV")
+  })
+
+  it("supports undo", () => {
+    let s = new DummyServer
+    s.type(0, "A")
+    s.type(1, "B")
+    s.type(0, "C")
+    s.undo(1)
+    s.conv("AC")
+    s.type(1, "D")
+    s.type(0, "E")
+    s.conv("ACDE")
+  })
+
+  it("supports redo", () => {
+    let s = new DummyServer
+    s.type(0, "A")
+    s.type(1, "B")
+    s.type(0, "C")
+    s.undo(1)
+    s.redo(1)
+    s.type(1, "D")
+    s.type(0, "E")
+    s.conv("ABCDE")
+  })
+
+  it("supports deep undo", () => {
+    let s = new DummyServer(doc(p("hello"), p("bye")))
+    s.update(0, sel(6))
+    s.update(1, sel(11))
+    s.type(0, "!")
+    s.type(1, "!")
+    s.update(0, closeHist)
+    s.delay(0, () => {
+      s.type(0, " ...")
+      s.type(1, " ,,,")
+    })
+    s.update(0, closeHist)
+    s.type(0, "*")
+    s.type(1, "*")
+    s.undo(0)
+    s.conv(doc(p("hello! ..."), p("bye! ,,,*")))
+    s.undo(0)
+    s.undo(0)
+    s.conv(doc(p("hello"), p("bye! ,,,*")))
+    s.redo(0)
+    s.redo(0)
+    s.redo(0)
+    s.conv(doc(p("hello! ...*"), p("bye! ,,,*")))
+    s.undo(0)
+    s.undo(0)
+    s.conv(doc(p("hello!"), p("bye! ,,,*")))
+    s.undo(1)
+    s.conv(doc(p("hello!"), p("bye")))
+  })
+
+  it("support undo with clashing events", () => {
+    let s = new DummyServer(doc(p("hello")))
+    s.update(0, sel(6))
+    s.type(0, "A")
+    s.delay(0, () => {
+      s.type(0, "B", 4)
+      s.type(0, "C", 5)
+      s.type(0, "D", 1)
+      s.update(1, s => s.tr.delete(2, 5).action())
+    })
+    s.conv("DhoA")
+    s.undo(0)
+    s.undo(0)
+    s.conv("ho")
+    ist(s.states[0].selection.head, 3)
+  })
+})
---
+++ test/test-rebase.js
@@ -0,0 +1,168 @@
+const {Transform} = require("prosemirror-transform")
+const ist = require("ist")
+const {schema, sameDoc, doc, blockquote, p, li, ul, em} = require("prosemirror-model/test/build")
+
+const {rebaseSteps} = require("../src/collab")
+
+function runRebase(transforms, expected) {
+  let start = transforms[0].before, full = new Transform(start)
+  transforms.forEach(transform => {
+    let rebased = new Transform(transform.doc)
+    let start = rebaseSteps(rebased, transform.steps,
+                            transform.steps.map((s, i) => s.invert(transform.docs[i])),
+                            full.steps)
+    for (let i = start; i < rebased.steps.length; i++) full.step(rebased.steps[i])
+  })
+  
+  ist(full.doc, expected, sameDoc)
+
+  for (let tag in start.tag) {
+    let mapped = full.mapping.mapResult(start.tag[tag])
+
+    let exp = expected.tag[tag]
+    if (mapped.deleted) {
+      if (exp) throw new Error("Tag " + tag + " was unexpectedly deleted")
+    } else {
+      if (!exp) throw new Error("Tag " + tag + " is not actually deleted")
+      ist(mapped.pos, exp)
+    }
+  }
+}
+
+function permute(array) {
+  if (array.length < 2) return [array]
+  let result = []
+  for (let i = 0; i < array.length; i++) {
+    let others = permute(array.slice(0, i).concat(array.slice(i + 1)))
+    for (let j = 0; j < others.length; j++)
+      result.push([array[i]].concat(others[j]))
+  }
+  return result
+}
+
+function rebase(doc, ...clients) {
+  let expected = clients.pop()
+  runRebase(clients.map(cl => cl(new Transform(doc))), expected)
+}
+
+function rebase$(doc, ...clients) {
+  let expected = clients.pop()
+  permute(clients.map(cl => cl(new Transform(doc)))).forEach(transforms => runRebase(transforms, expected))
+}
+
+function type(tr, pos, text) {
+  return tr.replaceWith(pos, pos, schema.text(text))
+}
+
+function wrap(tr, pos, type) {
+  let $pos = tr.doc.resolve(pos)
+  return tr.wrap($pos.blockRange($pos), [{type: schema.nodes[type]}])
+}
+
+describe("rebaseSteps", () => {
+  it("supports concurrent typing", () => {
+    rebase$(doc(p("h<1>ell<2>o")),
+            tr => type(tr, 2, "X"),
+            tr => type(tr, 5, "Y"),
+            doc(p("hX<1>ellY<2>o")))
+  })
+
+  it("support multiple concurrently typed chars", () => {
+    rebase$(doc(p("h<1>ell<2>o")),
+            tr => type(type(type(tr, 2, "X"), 3, "Y"), 4, "Z"),
+            tr => type(type(tr, 5, "U"), 6, "V"),
+            doc(p("hXYZ<1>ellUV<2>o")))
+  })
+
+  it("supports three concurrent typers", () => {
+    rebase$(doc(p("h<1>ell<2>o th<3>ere")),
+            tr => type(tr, 2, "X"),
+            tr => type(tr, 5, "Y"),
+            tr => type(tr, 9, "Z"),
+            doc(p("hX<1>ellY<2>o thZ<3>ere")))
+  })
+
+  it("handles wrapping of changed blocks", () => {
+    rebase$(doc(p("<1>hell<2>o<3>")),
+            tr => type(tr, 5, "X"),
+            tr => wrap(tr, 1, "blockquote"),
+            doc(blockquote(p("<1>hellX<2>o<3>"))))
+  })
+
+  it("handles insertions in deleted content", () => {
+    rebase$(doc(p("hello<1> wo<2>rld<3>!")),
+            tr => tr.delete(6, 12),
+            tr => type(tr, 9, "X"),
+            doc(p("hello<1><3>!")))
+  })
+
+  it("allows deleting the same content twice", () => {
+    rebase(doc(p("hello<1> wo<2>rld<3>!")),
+           tr => tr.delete(6, 12),
+           tr => tr.delete(6, 12),
+           doc(p("hello<1><3>!")))
+  })
+
+  it("isn't confused by joining a block that's being edited", () => {
+    rebase$(doc(ul(li(p("one")), "<1>", li(p("tw<2>o")))),
+            tr => type(tr, 12, "A"),
+            tr => tr.join(8),
+            doc(ul(li(p("one"), p("twA<2>o")))))
+  })
+
+  it("supports typing concurrently with marking", () => {
+    rebase(doc(p("hello <1>wo<2>rld<3>")),
+           tr => tr.addMark(7, 12, schema.mark("em")),
+           tr => type(tr, 9, "_"),
+           doc(p("hello <1>", em("wo"), "_<2>", em("rld<3>"))))
+  })
+
+  it("doesn't unmark marks added concurrently", () => {
+    rebase(doc(p(em("<1>hello"), " world<2>")),
+           tr => tr.addMark(1, 12, schema.mark("em")),
+           tr => tr.removeMark(1, 12, schema.mark("em")),
+           doc(p("<1>hello", em(" world<2>"))))
+  })
+
+  it("doesn't mark concurrently unmarked text", () => {
+    rebase(doc(p("<1>hello ", em("world<2>"))),
+           tr => tr.removeMark(1, 12, schema.mark("em")),
+           tr => tr.addMark(1, 12, schema.mark("em")),
+           doc(p(em("<1>hello "), "world<2>")))
+  })
+
+  it("deletes inserts in replaced context", () => {
+    rebase(doc(p("b<before>efore"), blockquote(ul(li(p("o<1>ne")), li(p("t<2>wo")), li(p("thr<3>ee")))), p("a<after>fter")),
+           tr => tr.replace(tr.doc.tag[1], tr.doc.tag[3], doc(p("a"), blockquote(p("b")), p("c")).slice(2, 9)),
+           tr => type(tr, tr.doc.tag[2], "ayay"),
+           doc(p("b<before>efore"), blockquote(ul(li(p("o<1>"), blockquote(p("b")), p("<3>ee")))), p("a<after>fter")))
+  })
+
+  it("maps through inserts", () => {
+    rebase$(doc(p("X<1>X<2>X")),
+            tr => type(tr, 2, "hello"),
+            tr => type(tr, 3, "goodbye").delete(4, 7),
+            doc(p("Xhello<1>Xgbye<2>X")))
+  })
+
+  it("handle concurrent removal of blocks", () => {
+    rebase(doc(p("a"), "<1>", p("b"), "<2>", p("c")),
+           tr => tr.delete(tr.doc.tag[1], tr.doc.tag[2]),
+           tr => tr.delete(tr.doc.tag[1], tr.doc.tag[2]),
+           doc(p("a"), "<1><2>", p("c")))
+  })
+
+  it("discards edits in removed blocks", () => {
+    rebase$(doc(p("a"), "<1>", p("b<2>"), "<3>", p("c")),
+            tr => tr.delete(tr.doc.tag[1], tr.doc.tag[3]),
+            tr => type(tr, tr.doc.tag[2], "ay"),
+            doc(p("a"), "<1><3>", p("c")))
+  })
+
+  it("preserves double block inserts", () => {
+    rebase(doc(p("a"), "<1>", p("b")),
+           tr => tr.replaceWith(3, 3, schema.node("paragraph")),
+           tr => tr.replaceWith(3, 3, schema.node("paragraph")),
+           doc(p("a"), p(), p(), "<1>", p("b")))
+  })
+})
