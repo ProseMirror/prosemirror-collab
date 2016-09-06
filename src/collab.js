cea28f0fc5486f112540986a7703fdb1ff066893
Marijn Haverbeke <marijn@haverbeke.nl>
2016-09-06 15:38:04 +0200

    Move code into src/

--- src/collab.js
+++ src/collab.js
@@ -1,7 +1,20 @@
-const {Plugin} = require("../state")
+const {Plugin} = require("prosemirror-state")
 const RopeSequence = require("rope-sequence")
 
-const {rebaseSteps} = require("./rebase")
+// : (Transform, [Step], [Step], [Step]) → number
+// Undo a given set of steps, apply a set of other steps, and then
+// redo them.
+function rebaseSteps(transform, steps, inverted, inside) {
+  for (let i = inverted.length - 1; i >= 0; i--) transform.step(inverted[i])
+  for (let i = 0; i < inside.length; i++) transform.step(inside[i])
+  for (let i = 0, mapFrom = inverted.length; i < steps.length; i++) {
+    let mapped = steps[i].map(transform.mapping.slice(mapFrom))
+    mapFrom--
+    if (mapped && !transform.maybeStep(mapped).failed)
+      transform.mapping.setMirror(mapFrom, transform.steps.length - 1)
+  }
+  return inverted.length + inside.length
+}
 exports.rebaseSteps = rebaseSteps
 
 // This state field accumulates changes that have to be sent to the
